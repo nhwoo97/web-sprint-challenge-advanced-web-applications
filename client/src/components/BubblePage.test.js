9719a8c6d63e1911b459857ba244f55773bbc4d5
Namhyuk Woo <namhyukwoo@Namhyuks-Air.lan>
2020-09-18 10:01:28 -0700

    done

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,39 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+
+import { render } from "@testing-library/react";
+
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+import { fetchColorsApi as mockFetchColorsApi } from "../api/fetchColors";
+
+jest.mock("../api/fetchColors");
+
+const colorList = {
+  data: [
+    {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 1,
+    },
+    {
+      color: "red",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 2,
+    },
+  ],
+};
+
+test("gets data and renders some bubbles", async () => {
+  mockFetchColorsApi.mockResolvedValueOnce(colorList);
+
+  const { getByText } = render(<BubblePage />);
+  const colors = getByText(/colors/i);
+  expect(colors).toBeInTheDocument();
+
+  const bubbles = getByText(/bubbles/i);
+  expect(bubbles).toBeInTheDocument();
 });
